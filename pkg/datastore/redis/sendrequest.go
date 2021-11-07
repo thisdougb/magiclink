c206795e0ba93be1551959ca6e8580d1c81ab5b1
thisdougb <thisdougb@users.noreply.github.com>
2021-11-07 13:22:36 +0000

    Adds redis key prefix env var to sendqueue key.
    Fixes #4.

--- pkg/datastore/redis/sendrequest.go
+++ pkg/datastore/redis/sendrequest.go
@@ -7,16 +7,21 @@ import (
 
 func (d *Datastore) SubmitSendLinkRequest(data string) error {
 
+	var cfg *config.Config // dynamic config settings
+
+	key := fmt.Sprintf("%s%s", cfg.REDIS_KEY_PREFIX(), linkSendQueue)
+
 	// push to FIFO queue, so the notify process will send an email
-	return d.lpushToList(linkSendQueue, data)
+	return d.lpushToList(key, data)
 }
 
 func (d *Datastore) StoreAuthID(email string, id string, ttlSeconds int) error {
 
+	var cfg *config.Config // dynamic config settings
+
 	// we set to the key with a ttl, so it auto-magically cleans up. only the email
 	// is required.
 
-	var cfg *config.Config // dynamic config settings
 	key := fmt.Sprintf("%s%s:%s", cfg.REDIS_KEY_PREFIX(), authIDsKey, id)
 
 	return d.setWithExpiry(key, email, ttlSeconds)
