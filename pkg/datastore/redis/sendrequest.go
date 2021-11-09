6d0a1c790dc31876d3e6a810acd016b52e3f6b81
Doug Bridgens <thisdougb@users.noreply.github.com>
2021-11-09 16:18:03 +0000

    Fixes the login rate-limiter checks.  Fixes #12.

--- pkg/datastore/redis/sendrequest.go
+++ pkg/datastore/redis/sendrequest.go
@@ -40,33 +40,36 @@ func (d *Datastore) GetLoginAttempts(email string, ttlMinutes int) ([]string, er
 	key := fmt.Sprintf(LoginRequestsKeyFormat, email)
 	key = fmt.Sprintf("%s%s", cfg.ValueAsStr("REDIS_KEY_PREFIX"), key)
 
-	// A secondary check for denial of service attempts. If there already too many
-	// login requests don't continue. I don't know how to do this better, before
-	// entering the pipeline (which will always add another login attempt)
-	result, err := d.client.ZRangeByScore(d.ctx, key, &redis.ZRangeBy{
-		Min:    "-inf",
-		Max:    "+inf",
-		Offset: 0,
-		Count:  -1,
-	}).Result()
-	if err != nil {
-		return logins, err
-	}
-	if len(result) > cfg.ValueAsInt("RATE_LIMIT_MAX_SEND_REQUESTS") {
-		return logins, errors.New("too many requests")
-	}
-
 	now := time.Now().UnixNano() / 1e6 // convert to milliseconds
 	nowStr := strconv.FormatInt(now, 10)
 
 	since := now - int64((ttlMinutes * 60 * 1000)) // convert to milliseconds
 	sinceStr := strconv.FormatInt(since, 10)
 
-	// BEGIN CRITICAL SECTION -----
+	// batch Redis commands
 	pipe := d.client.TxPipeline()
 
 	// housekeeping, remove old login attempts - do this here simply for general efficiency
-	_ = pipe.ZRemRangeByScore(d.ctx, key, "-inf", sinceStr)
+	d.client.ZRemRangeByScore(d.ctx, key, "-inf", sinceStr)
+
+	// An initial check for denial of service attempts. If there already too many
+	// login requests don't continue. Don't add more set members, which would just
+	// consume more memory.
+	result := pipe.ZRangeByScore(d.ctx, key, &redis.ZRangeBy{
+		Min:    "-inf",
+		Max:    "+inf",
+		Offset: 0,
+		Count:  -1,
+	})
+
+	if len(result.Val()) > cfg.ValueAsInt("RATE_LIMIT_MAX_SEND_REQUESTS") {
+		return logins, errors.New("too many requests")
+	}
+
+	_, err := pipe.Exec(d.ctx)
+	if err != nil {
+		return logins, err
+	}
 
 	// now read the recent login attempts
 	values := pipe.ZRangeByScore(d.ctx, key, &redis.ZRangeBy{
@@ -86,7 +89,6 @@ func (d *Datastore) GetLoginAttempts(email string, ttlMinutes int) ([]string, er
 	if err != nil {
 		return logins, err
 	}
-	// END CRITICAL SECTION -----
 
 	for _, value := range values.Val() {
 		logins = append(logins, value)
